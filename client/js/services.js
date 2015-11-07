c0e57c18e17787481071a1e0b2bc75306c21d982
Yilin Xu <yx296@nyu.edu>
2015-11-06 21:42:15 -0800

    Refactor openWeatherAPI service

--- client/js/services.js
+++ client/js/services.js
@@ -2,14 +2,15 @@ angular.module('weather.services', [])
 
 .factory('openWeatherAPI', function($http) {
   
-  var openWeatherAPI = {
-    getWeatherForCity: function (city) {
-      return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=693b2826cea96ecff31ad27e7dd55247')
-        .then(function(response) {
-          return response.data;
-        });   
-    }
-  };
+  var openWeatherAPI = {};
+
+  openWeatherAPI.getWeatherForCity = function (city) {
+    return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=693b2826cea96ecff31ad27e7dd55247')
+      .then(function(response) {
+        return response.data;
+      });   
+  }
+  
 
   return openWeatherAPI;
 })
